8c77ce1c09a382da0acb3bdc4e1083e06058ec89
adheesh <adheeshzkp@gmail.com>
2025-10-29 01:06:18 +0530

    Fixed Settings.py and Created url links between apps

--- server/distribution_server/distribution_server/settings.py
+++ server/distribution_server/distribution_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crud_service',
+    'recommendation_service',
 ]
 
 MIDDLEWARE = [
